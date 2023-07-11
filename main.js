f1b3165089fbbc720f1b6e3e872eb662a50cf1d5
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-07-11 15:53:14 +0500

    linters added

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 let books = [];
 let book = {};
 
-const listSection = document.getElementById("list");
+const listSection = document.getElementById('list');
 
 const booksList = () => {
-  let tag = document.querySelector(".list");
+  let tag = document.querySelector('.list');
   let child = tag.lastElementChild;
   while (child) {
     tag.removeChild(child);
     child = tag.lastElementChild;
   }
-  const booksData = JSON.parse(localStorage.getItem("books"));
+  const booksData = JSON.parse(localStorage.getItem('books'));
   booksData.map((book, index) => {
-    const oneBook = document.createElement("div");
+    const oneBook = document.createElement('div');
     //   oneBook.classList.add('books_data');
     oneBook.innerHTML = `
     <span>"${book.title}"</span>
@@ -24,22 +24,22 @@ const booksList = () => {
 };
 
 const add = () => {
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
 
   book = {};
-  book["title"] = title;
-  book["author"] = author;
+  book['title'] = title;
+  book['author'] = author;
   books.push(book);
   const booksData = JSON.stringify(books);
-  localStorage.setItem("books", booksData);
+  localStorage.setItem('books', booksData);
   booksList();
 };
 
 const remove = (index) => {
-  const booksData = JSON.parse(localStorage.getItem("books"));
+  const booksData = JSON.parse(localStorage.getItem('books'));
   booksData.splice(index, 1);
   const bData = JSON.stringify(booksData);
-  localStorage.setItem("books", bData);
+  localStorage.setItem('books', bData);
   booksList();
 };
