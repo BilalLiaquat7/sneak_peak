aa660270d231f15ab72e745f1d617b1f75755488
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-07-13 14:40:45 +0500

    converted to class

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-let books = [];
-let book = {};
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
-const listSection = document.getElementById('list');
+const books = [];
 
-const booksList = () => {
-  let tag = document.querySelector('.list');
+AddBook = (book) => {
+  if (localStorage.getItem("books") === null) {
+    localStorage.setItem("books", JSON.stringify([]));
+  }
+  books.push(book);
+  const booksData = JSON.stringify(books);
+  console.log(booksData);
+  localStorage.setItem("books", booksData);
+};
+
+displayBooksList = () => {
+  const listSection = document.getElementById("list");
+
+  let tag = document.querySelector(".list");
   let child = tag.lastElementChild;
   while (child) {
     tag.removeChild(child);
     child = tag.lastElementChild;
   }
-  const booksData = JSON.parse(localStorage.getItem('books'));
+  const booksData = JSON.parse(localStorage.getItem("books"));
   booksData.map((book, index) => {
-    const oneBook = document.createElement('div');
+    const oneBook = document.createElement("div");
     //   oneBook.classList.add('books_data');
     oneBook.innerHTML = `
     <span>"${book.title}"</span>
@@ -23,23 +39,23 @@ const booksList = () => {
   });
 };
 
-const add = () => {
-  let title = document.getElementById('title').value;
-  let author = document.getElementById('author').value;
-
-  book = {};
-  book['title'] = title;
-  book['author'] = author;
-  books.push(book);
-  const booksData = JSON.stringify(books);
-  localStorage.setItem('books', booksData);
-  booksList();
-};
-
-const remove = (index) => {
-  const booksData = JSON.parse(localStorage.getItem('books'));
+remove = (index) => {
+  const booksData = JSON.parse(localStorage.getItem("books"));
   booksData.splice(index, 1);
   const bData = JSON.stringify(booksData);
-  localStorage.setItem('books', bData);
-  booksList();
+  localStorage.setItem("books", bData);
+  displayBooksList();
 };
+
+const btnAdd = document.getElementById("btnAdd");
+
+btnAdd.addEventListener("click", () => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+
+  if (title !== "" && author !== "") {
+    const newBook = new Book(title, author);
+    AddBook(newBook);
+    displayBooksList();
+  }
+});
