4d746de6832e57b8a714d474761b39e1cbb8fbb1
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-07-13 15:25:03 +0500

    linters correction again

--- main.js
+++ main.js
@@ -7,39 +7,39 @@ class Book {
 
 const books = [];
 
-AddBook = (book) => {
+const AddBook = (book) => {
   if (localStorage.getItem('books') === null) {
     localStorage.setItem('books', JSON.stringify([]));
   }
   books.push(book);
   const booksData = JSON.stringify(books);
-  console.log(booksData);
   localStorage.setItem('books', booksData);
 };
 
-displayBooksList = () => {
+const displayBooksList = () => {
   const listSection = document.getElementById('list');
 
-  let tag = document.querySelector('.list');
+  const tag = document.querySelector('.list');
   let child = tag.lastElementChild;
   while (child) {
     tag.removeChild(child);
     child = tag.lastElementChild;
   }
   const booksData = JSON.parse(localStorage.getItem('books'));
+  // eslint-disable-next-line no-unused-vars
   booksData.map((book, index) => {
     const oneBook = document.createElement('div');
-    //   oneBook.classList.add('books_data');
     oneBook.innerHTML = `
     <span>"${book.title}"</span>
     <span>"${book.author}"</span>
     <button type="button" onclick="remove(${index})">Remove</button>`;
 
     listSection.appendChild(oneBook);
+    return oneBook;
   });
 };
-
-remove = (index) => {
+// eslint-disable-next-line no-unused-vars
+const remove = (index) => {
   const booksData = JSON.parse(localStorage.getItem('books'));
   booksData.splice(index, 1);
   const bData = JSON.stringify(booksData);
@@ -58,4 +58,6 @@ btnAdd.addEventListener('click', () => {
     AddBook(newBook);
     displayBooksList();
   }
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
 });
